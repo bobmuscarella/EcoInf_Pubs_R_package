6139a93b189ffbd8a9729e6317eb61e478108e63
Bob Muscarella <bob.muscarella@gmail.com>
2021-11-01 09:50:20 +0100

    Update send.submitted.email.R

--- EcoinfPubsRpackage/R/send.submitted.email.R
+++ EcoinfPubsRpackage/R/send.submitted.email.R
@@ -5,12 +5,12 @@ function(ArchiveFile=NULL, DraftOnly=FALSE, SendersEmail=NULL, Deadline=NULL, Sa
   submitted <- pubs[pubs$Status %in% "Submitted" & !is.na(pubs$Status0), ]
   dl <- ifelse(is.null(Deadline), "", paste("\n\nPlease respond by", Deadline, "to ensure the update gets into the next newsletter"))
   
-  for(i in 1:length(unique(submitted$Contact.Email))){
-    titles <- submitted$Title[submitted$Contact.Email %in% unique(submitted$Contact.Email)[i]]
-    urls <- submitted$X[submitted$Contact.Email %in% unique(submitted$Contact.Email)[i]]
+  for(i in 1:length(unique(submitted$'Contact Email'))){
+    titles <- submitted$Title[submitted$'Contact Email' %in% unique(submitted$'Contact Email')[i]]
+    urls <- submitted$'...10'[submitted$'Contact Email' %in% unique(submitted$'Contact Email')[i]]
     txt <- paste0("Hello!\nJust a reminder that you can update the status of your submitted article(s) when they change status using the link(s) below:\n\n", paste0(titles, "\n", urls, collapse="\n\n"), dl, "\n\n", Salutation)
     
-    tmp_email <- mime(To = as.character(unique(submitted$Contact.Email)[i]), 
+    tmp_email <- mime(To = as.character(unique(submitted$'Contact Email')[i]), 
                     From = SendersEmail, 
                     Subject = "EcoInf publications list", 
                     body = txt)
